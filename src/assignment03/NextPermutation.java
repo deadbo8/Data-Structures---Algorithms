7f6832ff909a9ef41edc69184cfe85cb558bc3bc
Akshant <piratedlif3@gmail.com>
2025-07-10 10:41:26 +0530

    Fixes next permutation generation
    Corrects the next permutation logic to properly generate the next permutation in lexicographical order by swapping the last two elements of a sorted array.
    
    Addresses an issue where the permutation was not being generated correctly.

--- src/assignment03/NextPermutation.java
+++ src/assignment03/NextPermutation.java
@@ -1,13 +1,35 @@
 package assignment03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NextPermutation {
 
 	public static void main(String[] args) {
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
+		int n = s.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		if (arr.length > 1) {
+			int temp = arr[n - 1];
+			arr[n - 1] = arr[n - 2];
+			arr[n - 2] = temp;
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
 		s.close();
 
 	}
--- src/leetcode/Generate_Paranthesis_22_recursion.java
+++ src/leetcode/Generate_Paranthesis_22_recursion.java
@@ -46,54 +46,51 @@ public class Generate_Paranthesis_22_recursion {
 
 	}
 
-	public static List<String> generateV1(int n){
-		
-		
+	public static List<String> generateV1(int n) {
+
 		List<String> build = new ArrayList<>();
-		
-		int open=0;
-		int close=0;
-		
-		String s ="";
-		
-		helper(n,open, close, build, s);
-		
+
+		int open = 0;
+		int close = 0;
+
+		String s = "";
+
+		helper(n, open, close, build, s);
+
 		return build;
 
 	}
-	
+
 	public static void helper(int n, int open, int close, List<String> build, String s) {
-		
-		
-		if(close>open) {
+
+		if (close > open) {
 			return;
 		}
-		if(s.length()==2*n) {
+		if (s.length() == 2 * n) {
 			build.add(s);
 			return;
 		}
-		
+
 		if (open < n) {
-			helper(n,open+1, close, build, s + "(");
+			helper(n, open + 1, close, build, s + "(");
 		}
-		if (close < n) {
-			helper(n,open, close+1, build, s + ")");
+		if (close < open) {
+			helper(n, open, close + 1, build, s + ")");
 		}
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		
+
 		List<String> paranthesis = new ArrayList<>();
 
 //		generate(n, "");
-		
-		paranthesis=generateV1(n);
-		
+
+		paranthesis = generateV1(n);
+
 		System.out.println(paranthesis);
 
 		s.close();
