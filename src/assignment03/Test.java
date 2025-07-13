5447f32ad7594524218c015a39057360fe7ae4d8
Akshant <piratedlif3@gmail.com>
2025-07-13 12:48:29 +0530

    Adds rat chases cheese algorithm
    Implements an algorithm to find a path for a rat to chase cheese in a maze.
    
    The algorithm recursively explores possible paths, marking visited cells and backtracking when necessary.
    A test class was added and then removed and is no longer needed.

--- src/assignment03/Test.java
+++ src/assignment03/Test.java
@@ -1,17 +1,22 @@
 package assignment03;
 
 public class Test {
+	
+	public static int modify(int a) {
+		return a=1; 
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		System.out.println(Math.pow(2.00, -2));
 		
-		char c = '2';
+		int a = 0; 
 		
-		int t =c-'0';
+		a= modify(a);
+		System.out.println(a);
 			
-			System.out.println(t);
-		
+			
 		
 		}
 		
--- src/codeSkiller/RatChasesIytsCheese.java
+++ src/codeSkiller/RatChasesIytsCheese.java
@@ -1,5 +1,59 @@
 package codeSkiller;
 
+import java.util.Scanner;
+
 public class RatChasesIytsCheese {
 
+	public static void path(char[][] maze, int[][] res, int row, int col, int n , int m) {
+		
+		if(row<0 || col<0 || row>=n || col>=m || maze[row][col]=='x' ) {
+			return; 
+		}
+		
+		if(row == n-1 && col == m-1 ) {
+			print(res);
+			res[row][col]=1;
+			return;
+		}
+		
+		maze[row][col]='x';
+		path(maze,res,row-1,col,n,m);
+		path(maze,res,row-1,col,n,m);
+		path(maze,res,row-1,col,n,m);
+		path(maze,res,row-1,col,n,m);
+		res[row][col]=0;
+		maze[row][col]='o';
+	
+	}
+	
+	public static void print(int[][] res) {
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[0].length; j++) {
+				System.out.print(res[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int n = s.nextInt();
+		int m = s.nextInt();
+
+		char[][] maze = new char[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				maze[i][j] = s.next().charAt(0);
+			}
+		}
+
+		int[][] res = new int[n][m];
+		
+		path(maze, res,0,0,n,m);
+
+
+		s.close();
+	}
 }
